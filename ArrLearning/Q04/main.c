e6911e04dc6e294baaedb5a32d3f424a8a05b299
Artyom Kiselhof <artyomkiselhof2007@gmail.com>
2025-08-04 18:27:50 +0300

    4-5

--- ArrLearning/Q04/main.c
+++ ArrLearning/Q04/main.c
@@ -1,1 +1,23 @@
-// הגדר מערך נוסף מטיפוס שלם בן 10 איברים והעבר אליו את המערך שבשאלה 3
+// Declare another integer array of 10 elements and copy into it the array from question 3
+#include <stdio.h>
+
+int main() {
+    int arr[10];
+    int arrCopy[10];
+    
+    for (int i = 0; i < 10; i++){
+        arr[i] = 10 - i;
+    }
+
+    for (int i = 0; i < 10; i++){
+        arrCopy[i] = arr[i];
+    }
+
+    for (int i = 0; i < 10; i++){
+        printf("%d ", arrCopy[i]); 
+    }
+
+    printf("\n");
+    
+    return 0;
+}
---
+++ ArrLearning/Q05/main.c
@@ -0,0 +1,39 @@
+// Define two arrays of 5 real (float) elements, input values for both arrays. Then swap the contents of the two arrays.
+#include <stdio.h>
+
+int main() {
+    float arr[5];
+    float arrSecond[5];
+
+    printf("First arr: \n");
+    for (int i = 0; i < 5; i++){
+        printf("EnterNum in First arr: ");
+        scanf("%f", &arr[i]);
+    }
+
+    printf("Second arr: \n");
+    for (int i = 0; i < 5; i++){
+        printf("EnterNum in Second arr: ");
+        scanf("%f", &arrSecond[i]);
+    }
+
+    for (int i = 0; i < 5; i++){
+        float temp = arr[i];
+        arr[i] = arrSecond[i];
+        arrSecond[i] = temp;
+    }
+
+    printf("First array after swap:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%.2f ", arr[i]);
+    }
+    printf("\n");
+
+    printf("Second array after swap:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%.2f ", arrSecond[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
