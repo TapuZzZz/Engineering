993e90ef15585178bf418b9c4fe86b41765b7fa7
Artyom Kiselhof <artyomkiselhof2007@gmail.com>
2025-12-26 11:37:26 +0200

    Add doubly linked list implementation and remove tree structure

---
+++ DataStructures/5_Node_DLLL.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next; 
+    struct Node* prev; 
+} Node;
+
+void Init_DLLL(Node** manager) {
+    *manager = NULL;
+}
+
+void Push_DLLL(Node** manager, int value) {
+    Node* newNode = (Node *)malloc(sizeof(Node));
+    if (!newNode) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    newNode->data = value;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    *manager = newNode;
+
+}
+
+void InsertPre_DLLL(Node* node, int value){
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (!newNode) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    newNode->prev = node->prev;
+    newNode->next = node;
+
+    node->prev->next = newNode;
+    node->prev = newNode;
+}
+
+//void InsertNext_DLLL(){
+    
+//}
+
+
+int main() {
+    Node* manager;
+    Push_DLLL(&manager, 4);
+
+    printf("%d\n", manager->data);
+
+    return 0;
+}
