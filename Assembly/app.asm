8e8b2ec39668e66c4c56a79b1e4f4e3f21c2343f
TapuZzZz <artyomkiselhof2007@gmail.com>
2026-04-14 02:56:28 +0300

    Refactor app.asm to remove factorial calculation and implement even number counting

--- Assembly/app.asm
+++ Assembly/app.asm
@@ -3,46 +3,32 @@ MODEL small
 STACK 100h
 
 DATASEG
-    num db 3          ; המספר שנרצה לחשב לו עצרת
-    result db ?       ; כאן תישמר התוצאה
+    arr dw 500, 300, 522, 133, 524, -1
+    cnt db 0 
 
 CODESEG
 start:
     mov ax, @data
     mov ds, ax
 
-    ; הכנת הפרמטר ב-AL וקריאה לפונקציה הרקורסיבית
-    mov al, [num]
-    call Factorial    ; התוצאה תחזור ב-AL
+    lea si, [arr]
+next_item:
+    mov ax, [si]
+    cmp ax, -1
+    je exit
 
-    ; שמירת התוצאה והדפסה (בהנחה שהיא חד ספרתית)
-    mov [result], al
-    mov dl, al
-    add dl, '0'
-    mov ah, 02h
-    int 21h
+    test ax, 1
+    jnz skip
+
+skip:
+
 
 exit:
     mov ax, 4c00h
     int 21h
-
-; --- פונקציה רקורסיבית לחישוב עצרת ---
-proc Factorial
-    cmp al, 1         ; תנאי עצירה: אם הגענו ל-1
-    jbe stop_rec      ; קפוץ לסיום הרקורסיה
-
-    push ax           ; שומרים את הערך הנוכחי (למשל 3) במחסנית
-    dec al            ; מורידים 1 (עכשיו AL הוא 2)
-    call Factorial    ; קריאה רקורסיבית: Factorial(n-1)
     
-    ; כשחוזרים מהקריאה, התוצאה של (n-1)! נמצאת ב-AL
-    pop bx            ; מוציאים מהמחסנית את הערך המקורי ששמרנו (למשל 3) לתוך BL
-    mul bl            ; מכפילים: AL = AL * BL
-    ret
+        
+    proc countEven 
 
-stop_rec:
-    mov al, 1         ; הבסיס: !1 = 1
-    ret
-endp Factorial
-
+    endp countEven
 END start
