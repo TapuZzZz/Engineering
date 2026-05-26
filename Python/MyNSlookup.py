fa239460fccfadd47579f745e67a2c4047c12de9
Artyom Kiselhof <artyomkiselhof2007@gmail.com>
2026-05-26 13:09:35 +0300

    changes in the nslookup

--- Python/MyNSlookup.py
+++ Python/MyNSlookup.py
@@ -1,29 +1,18 @@
 from scapy.all import IP, UDP, DNS, DNSQR, DNSRR, sr1
 
-# 1. יצירת החבילה עם שכבת ה-DNS והגדרת השאילתה (rd=1 אומר שזו שאילתה רקורסיבית)
-p = IP()/UDP()/DNS(rd=1)
+p = IP()/UDP()/DNS()
 
-# 2. הגדרת הפרמטרים
 p[IP].dst = "8.8.8.8"
 p[UDP].sport = 5555
 p[UDP].dport = 53
 
-# תיקון 1: כדי להגדיר את שם האתר, צריך ליצור אובייקט מסוג DNSQR בתוך ה-DNS
 p[DNS].qd = DNSQR(qname="www.one.co.il.")
 
-# 3. שליחת החבילה והמתנה לתשובה (sr1 מחזיר חבילה אחת)
-res = sr1(p)
+res = sr1(p, verbose=0) # משתיק את ההודעות של השליחה והקבלה
 
-# 4. הדפסת התשובות
 if res and res.haslayer(DNSRR):
-    # תיקון 2: מכיוון שיכולות לחזור כמה תשובות (Records), נרוץ עליהן בלולאה
-    print("\n--- IP Addresses Received ---")
-    
-    # סריקה של כל אובייקטי ה-DNSRR (תשובות) שחזרו בחבילה
     for i in range(res[DNS].ancount):
-        print(res[DNSRR][i].rdata)
+        answer = res[DNS].an[i]
         
-    print("\n--- Full Packet Show ---")
-    res.show()
-else:
-    print("No response received or no DNS Answer found.")
+        if answer.type == 1:
+            print(answer.rdata)
