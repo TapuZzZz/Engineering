32a0562d600c3f8c172f54e2694db391dd83682c
TapuZzZz <artyomkiselhof2007@gmail.com>
2026-06-28 04:30:59 +0300

    change

--- Full-Stack/FormApp/src/app/app.ts
+++ Full-Stack/FormApp/src/app/app.ts
@@ -1,17 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-
 export class App {
-  userName = "";
-  switch = false;
-  ageMin = 18;
-  myAge = null;
+  protected readonly title = signal('FormApp');
 }
